e1aa2ba9886a457b0ffc8cfbf81ca7d848f3eed1
ParadoxNinja <91133442+ParadoxNinja@users.noreply.github.com>
2022-04-07 18:43:36 +0530

    4/7/2022 commit C

--- demoScript1.py
+++ demoScript1.py
@@ -1,8 +1,8 @@
 import sqlite3 as sql
-
-conn = sql.connect("lite.db")
-cur = conn.cursor()
-cur.execute("CREATE TABLE  IF NOT EXISTS store (item TEXT,  quantity INTEGER, price REAL")
-cur.execute("INSERT INTO store VALUES('Chocolate', 8, 15.0)")
-conn.commit()
-conn.close()
+def create_table():
+    conn = sql.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE  IF NOT EXISTS store (item TEXT,  quantity INTEGER, price REAL")
+    cur.execute("INSERT INTO store VALUES('Chocolate', 8, 15.0)")
+    conn.commit()
+    conn.close()
